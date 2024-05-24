7941dfb14b6e5969dd4ec8d00c0b0d67fb1fbeea
MayurJabare <79738719+MayurJabare@users.noreply.github.com>
2024-05-24 09:23:28 +0530

    Update urls.py

--- projectone/doctorapp/urls.py
+++ projectone/doctorapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from doctorapp import views
 
+
+
 urlpatterns = [
-    # path('', views.handle, name='handle data'),
     path('', views.doctor_list, name='doctor list data'),
+    path('refresh_table', views.update_doctor_table, name='update doctor table data'),
 ]
+
